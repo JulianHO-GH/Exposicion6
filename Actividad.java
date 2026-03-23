4bae0caa844ab173393c9dee0eca78ef020bdeda
Julian <juliancitohernandez2006@gmail.com>
2026-03-23 18:45:49 -0500

    Addfiles

---
+++ Actividad.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import javax.swing.*;
+/**
+ *
+ * @author INGRESE SU NOMBRE
+ */
+public class Actividad {
+    public static void main(String args[])
+    {
+        String nombre = //COMPLETAR, HACER QUE EL USUARIO INGRESE SU NOMBRE
+        int edad = //COMPLETAR, HACER QUE EL USUARIO INGRESE SU EDAD
+        String texto = "Hola, mi nombre es " + nombre + ", tengo " + edad + " años, y esta es la actividad de la exposición 6.";
+        JOptionPane.showMessageDialog(null, texto);
+    }
+}
